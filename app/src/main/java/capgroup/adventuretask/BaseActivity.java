cd0c877f3f53c38955b74c1e537eec3c02a45c1e
Alex Lockhart <alexlockhart7@gmail.com>
2015-02-28 08:33:28 -1000

    Added a basic basic layout using the standard android dropdown navigation because it was easy.

---
+++ app/src/main/java/capgroup/adventuretask/BaseActivity.java
@@ -0,0 +1,41 @@
+package capgroup.adventuretask;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class BaseActivity extends ActionBarActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_base, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_stats) {
+            startActivity(new Intent(BaseActivity.this, Stats.class));
+        } else if (id == R.id.action_discoveries) {
+            startActivity(new Intent(BaseActivity.this, MainActivity.class));
+        } else if (id == R.id.action_quests) {
+            startActivity(new Intent(BaseActivity.this, MainActivity.class));
+        } else if (id == R.id.action_level_up) {
+            startActivity(new Intent(BaseActivity.this, MainActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/capgroup/adventuretask/Stats.java
+++ app/src/main/java/capgroup/adventuretask/Stats.java
@@ -4,16 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.content.Intent;
 
 
-public class MainActivity extends BaseActivity {
+public class Stats extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_stats);
     }
 }
